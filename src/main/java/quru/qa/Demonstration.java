3214f6bc1c514523b32c40bb7e8c912b3bcf21dc
Lisa <bear.bessie@gmail.com>
2021-11-27 05:51:39 +0300

    Second commit for DZ 9 (GIT)

--- src/main/java/quru/qa/Demonstration.java
+++ src/main/java/quru/qa/Demonstration.java
@@ -6,14 +6,16 @@ import quru.qa.java02.TryMyClasses02;
 public class Demonstration {
     public static void main(String[] args) {
 
-        // ДЗ по лекции "3. Основы Java."
+   /*     // ДЗ по лекции "3. Основы Java."
         TryMyClasses01.tryCandyClass();
         TryMyClasses01.tryCarClass();
 
         // ДЗ по лекции "Дополнительное занятие «Основы Java - продолжение»"
-        TryMyClasses02.tryHouseClass();
+        TryMyClasses02.tryHouseClass();*/
 
-        int a = 5;
-        System.out.println(a);
+        int a = 5,
+                b= 7;
+        System.out.println("a = "+a+", b = "+b);
+
     }
 }
